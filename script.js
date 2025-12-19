220a34bbd9b4917bd8cf737f19bec3c8dcdeac3f
Vinicius Aragão <55242418+bsqqq@users.noreply.github.com>
2025-12-19 03:50:37 -0300

    Remove console logs in CPF validation
    Comment out console logs for CPF validation process.

--- script.js
+++ script.js
@@ -5,51 +5,48 @@ async function verificarCPF() {
         alert("Por favor insira o CPF")
         return
     }
-    console.log(`CPF: ${cpf}`)
+    // console.log(`CPF: ${cpf}`)
     var array = cpf.split("")
-    console.log(`Array: ${array}`)
-    console.log(`Tamanho do array: ${array.length}`)
+    // console.log(`Array: ${array}`)
+    // console.log(`Tamanho do array: ${array.length}`)
     var eValido;
     var aux = 0;
     var aux2 = 0;
+  
+    // para o primeiro digito verificador;
     for(var i = 0; i < array.length - 2; i++) {
-        // para o primeiro digito verificador;
         aux += (array[i] * (i + 1))
     }
     // console.log(`Valor do aux: ${aux}`)
     var digver = aux % 11
-    console.log(`Primeiro digito verificador: ${digver}`)
+    // console.log(`Primeiro digito verificador: ${digver}`)
     if(digver == 10) {
         digver = 0
     }
 
+    // para o segundo digito verificador;
     for(var k = 0; k < array.length - 1; k++) {
-        // para o segundo digito verificador;
         aux2 += (array[k] * k)
     }
     // console.log(`Valor do aux2: ${aux2}`)
     var digver2 = aux2 % 11
-    console.log(`Segundo digito verificador: ${digver2} (se for = 10, substitui-se por 0)`)
+    // console.log(`Segundo digito verificador: ${digver2} (se for = 10, substitui-se por 0)`)
     if(digver2 == 10) {
         digver2 = 0
     }
 
-    if(array[9] == digver) {
-        if(array[10] == digver2) {
+    if(array[9] == digver && array[10] == digver2) {
+        // if(array[10] == digver2) {
             eValido = true
             console.log("CPF Válido")
             // alert("CPF Válido!")
-        } else {
-            eValido = false
-            console.log("CPF Inválido")
-            // alert("CPF Inválido...")
-        }
+        
     } else {
         eValido = false
         console.log("CPF Inválido")
         // alert("CPF Inválido...")
     }
-    console.log(eValido) 
+    // console.log(eValido) 
     return eValido
 }
 
